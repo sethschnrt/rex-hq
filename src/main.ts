5f17dd1ac1cab96d9c4a83b0163a774127903632
Rex <Rex@Seths-Mac-mini.local>
2026-02-19 20:40:53 -0600

    debug: enable physics debug overlay

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ const config: Phaser.Types.Core.GameConfig = {
     default: 'arcade',
     arcade: {
       gravity: { x: 0, y: 0 },
-      debug: false,
+      debug: true,
     },
   },
   input: {
